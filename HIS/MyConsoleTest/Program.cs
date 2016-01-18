6c7b9ffe478f74bbdd49d1012830e6f6a8503dcf
devmicrominds <microminds89@gmail.com>
2016-01-18 17:37:46 +0530

    Added MyConsoleTest Project
    Added MyConsoleTest Project

---
+++ HIS/MyConsoleTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using HIS.Data;
+using HIS.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleTest
+{
+    class Program
+    {
+        private static IRepositoryFactory factory;
+        static void Main(string[] args)
+        {
+            factory
+            var repository = factory.GetRepository<Repository<Resource>>();
+            var query = repository.GetQueryable();
+
+        }
+    }
+}
